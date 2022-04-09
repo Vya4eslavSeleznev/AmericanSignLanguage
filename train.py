19ef077301071cfb7027150a39e03bdc93f5df50
Vya4eslav111 <vya4eslav.seleznev@gmail.com>
2022-04-09 15:49:24 +0300

    Train file

---
+++ train.py
@@ -0,0 +1,187 @@
+# python train.py --epochs 10
+
+import pandas as pd
+import numpy as np
+import torch
+import random
+import albumentations
+import matplotlib.pyplot as plt
+import argparse
+import torch.nn as nn
+import torch.optim as optim
+import time
+import cv2
+import cnn_models
+
+from tqdm import tqdm
+from sklearn.model_selection import train_test_split
+from torch.utils.data import Dataset, DataLoader
+
+# construct the argument parser and parse the arguments
+parser = argparse.ArgumentParser()
+parser.add_argument('-e', '--epochs', default=10, type=int,
+                    help='number of epochs to train the model for')
+args = vars(parser.parse_args())
+
+
+def seed_everything(seed=42):
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+    torch.backends.cudnn.benchmark = True
+
+
+SEED = 42
+seed_everything(seed=SEED)
+
+
+# set computation device
+device = ('cuda:0' if torch.cuda.is_available() else 'cpu')
+
+print(f"Computation device: {device}")
+
+# read the data.csv file and get the image paths and labels
+df = pd.read_csv('../input/data.csv')
+X = df.image_path.values
+y = df.target.values
+
+(x_train, x_test, y_train, y_test) = (train_test_split(X, y,
+                                                       test_size=0.15, random_state=42))
+
+print(f"Training on {len(x_train)} images")
+print(f"Validating on {len(x_test)} images")
+
+
+# image dataset module
+class ASLImageDataset(Dataset):
+    def __init__(self, path, labels):
+        self.X = path
+        self.y = labels
+
+        # apply augmentations
+        self.aug = albumentations.Compose([
+            albumentations.Resize(224, 224, always_apply=True),
+        ])
+
+    def __len__(self):
+        return len(self.X)
+
+    def __getitem__(self, i):
+        image = cv2.imread(self.X[i])
+        image = self.aug(image=np.array(image))['image']
+        image = np.transpose(image, (2, 0, 1)).astype(np.float32)
+        label = int(self.y[i])
+        return torch.tensor(image, dtype=torch.float), torch.tensor(label, dtype=torch.long)
+
+
+train_data = ASLImageDataset(x_train, y_train)
+test_data = ASLImageDataset(x_test, y_test)
+
+# data loaders
+train_loader = DataLoader(train_data, batch_size=32, shuffle=True)
+test_loader = DataLoader(test_data, batch_size=32, shuffle=False)
+
+model = cnn_models.CustomCNN().to(device)
+print(model)
+
+# Find total parameters and trainable parameters
+total_params = sum(p.numel() for p in model.parameters())
+print(f"{total_params:,} total parameters.")
+total_trainable_params = sum(
+    p.numel() for p in model.parameters() if p.requires_grad)
+print(f"{total_trainable_params:,} training parameters.")
+
+# optimizer
+optimizer = optim.Adam(model.parameters(), lr=0.001)
+# loss function
+criterion = nn.CrossEntropyLoss()
+
+
+# validation function
+def validate(model, dataloader):
+    print('Validating')
+    model.eval()
+    running_loss = 0.0
+    running_correct = 0
+    with torch.no_grad():
+        for i, data in tqdm(enumerate(dataloader), total=int(len(test_data) / dataloader.batch_size)):
+            data, target = data[0].to(device), data[1].to(device)
+            outputs = model(data)
+            loss = criterion(outputs, target)
+
+            running_loss += loss.item()
+            _, predicts = torch.max(outputs.data, 1)
+            running_correct += (predicts == target).sum().item()
+
+        val_loss = running_loss / len(dataloader.dataset)
+        val_accuracy = 100. * running_correct / len(dataloader.dataset)
+        print(f'Val Loss: {val_loss:.4f}, Val Acc: {val_accuracy:.2f}')
+
+        return val_loss, val_accuracy
+
+
+# training function
+def fit(model, dataloader):
+    print('Training')
+    model.train()
+    running_loss = 0.0
+    running_correct = 0
+    for i, data in tqdm(enumerate(dataloader), total=int(len(train_data) / dataloader.batch_size)):
+        data, target = data[0].to(device), data[1].to(device)
+        optimizer.zero_grad()
+        outputs = model(data)
+        loss = criterion(outputs, target)
+        running_loss += loss.item()
+        _, predicts = torch.max(outputs.data, 1)
+        running_correct += (predicts == target).sum().item()
+        loss.backward()
+        optimizer.step()
+
+    train_loss = running_loss / len(dataloader.dataset)
+    train_accuracy = 100. * running_correct / len(dataloader.dataset)
+
+    print(f"Train Loss: {train_loss:.4f}, Train Acc: {train_accuracy:.2f}")
+
+    return train_loss, train_accuracy
+
+
+train_loss, train_accuracy = [], []
+val_loss, val_accuracy = [], []
+start = time.time()
+for epoch in range(args['epochs']):
+    print(f"Epoch {epoch + 1} of {args['epochs']}")
+    train_epoch_loss, train_epoch_accuracy = fit(model, train_loader)
+    val_epoch_loss, val_epoch_accuracy = validate(model, test_loader)
+    train_loss.append(train_epoch_loss)
+    train_accuracy.append(train_epoch_accuracy)
+    val_loss.append(val_epoch_loss)
+    val_accuracy.append(val_epoch_accuracy)
+end = time.time()
+
+print(f"{(end - start) / 60:.3f} minutes")
+
+# accuracy plots
+plt.figure(figsize=(10, 7))
+plt.plot(train_accuracy, color='green', label='train accuracy')
+plt.plot(val_accuracy, color='blue', label='validation accuracy')
+plt.xlabel('Epochs')
+plt.ylabel('Accuracy')
+plt.legend()
+plt.savefig('../outputs/accuracy.png')
+plt.show()
+
+# loss plots
+plt.figure(figsize=(10, 7))
+plt.plot(train_loss, color='orange', label='train loss')
+plt.plot(val_loss, color='red', label='validation loss')
+plt.xlabel('Epochs')
+plt.ylabel('Loss')
+plt.legend()
+plt.savefig('../outputs/loss.png')
+plt.show()
+
+# save the model to disk
+print('Saving model...')
+torch.save(model.state_dict(), '../outputs/model.pth')
